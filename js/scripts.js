7f1e598863f48fbd473f4ccc5d20ed1909dfd3c2
xurxogonzalez <info@webferrol.com>
2021-11-02 09:40:31 +0100

    Inicializado ejercicios js
    -Creado js. Realizados ejercicos para alumnos

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+"use strict";
+//Ejercicio 1
+
+//let nombrePila = "Xurxo";
+//console.log(nombrePila);
+
+
+//Ejercicio 2
+// nombrePila = "Jorge";
+// console.log(nombrePila);
+
+//Ejercicio 3
+//Me dice si comento variable que no está definida
+
+let operador1=3;
+operador1 = 4;
+let operador2=5;
+let resultado=0;
+
+resultado = operador1+operador2;
+
+
+
+console.log(resultado);
+
